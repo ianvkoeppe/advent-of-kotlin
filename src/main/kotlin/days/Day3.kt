fa9caeaeb0ef9b165985b480f3b43ff38450c431
Ian V Koeppe <ianvkoeppe@icloud.com>
2021-12-03 08:55:49 -0600

    Day 3

--- src/main/kotlin/days/Day3.kt
+++ src/main/kotlin/days/Day3.kt
@@ -2,10 +2,24 @@ package days
 
 object Day3 {
   fun partOne(lines: List<String>): Long {
-    return 0
+    val gamma = (0 until lines.first().length).map { i -> maxBitAtIndex(lines, i) }.joinToString("").toLong(2)
+    return gamma * ((1L shl lines.first().length) - gamma - 1)
   }
 
   fun partTwo(lines: List<String>): Long {
-    return 0
+    val oxygen = filterBinaryUntilOne(lines, Char::equals).toLong(2)
+    val co2 = filterBinaryUntilOne(lines, { a, b -> a != b }).toLong(2)
+    return oxygen * co2
+  }
+
+  private fun filterBinaryUntilOne(binaries: List<String>, comparator: (Char, Any?) -> Boolean, index: Int = 0): String {
+    if (binaries.size == 1) return binaries.first()
+
+    val criteria = maxBitAtIndex(binaries, index)
+    return filterBinaryUntilOne(binaries.filter { comparator(it[index], criteria) }, comparator, index + 1)
+  }
+
+  private fun maxBitAtIndex(binaries: List<String>, index: Int = 0): Char {
+    return if (binaries.map { it[index] }.count { it == '1' } >= binaries.size / 2.0) '1' else '0'
   }
 }
--- src/test/kotlin/days/Day3Tests.kt
+++ src/test/kotlin/days/Day3Tests.kt
@@ -7,21 +7,21 @@ import org.junit.jupiter.api.Test
 class Day3Tests {
   @Test
   fun partOneExample() {
-    assertThat(Day3.partOne(Reader.read("src/test/resources/day3/example.txt"))).isEqualTo(0)
+    assertThat(Day3.partOne(Reader.read("src/test/resources/day3/example.txt"))).isEqualTo(198)
   }
 
   @Test
   fun partOne() {
-    assertThat(Day3.partOne(Reader.read("src/test/resources/day3/problem.txt"))).isEqualTo(0)
+    assertThat(Day3.partOne(Reader.read("src/test/resources/day3/problem.txt"))).isEqualTo(775304)
   }
 
   @Test
   fun partTwoExample() {
-    assertThat(Day3.partTwo(Reader.read("src/test/resources/day3/example.txt"))).isEqualTo(0)
+    assertThat(Day3.partTwo(Reader.read("src/test/resources/day3/example.txt"))).isEqualTo(230)
   }
 
   @Test
   fun partTwo() {
-    assertThat(Day3.partTwo(Reader.read("src/test/resources/day3/problem.txt"))).isEqualTo(0)
+    assertThat(Day3.partTwo(Reader.read("src/test/resources/day3/problem.txt"))).isEqualTo(1370737)
   }
 }
