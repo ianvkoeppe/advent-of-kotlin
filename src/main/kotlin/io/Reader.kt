ef986ca036766cd8da3da0f08049d9cb14c4670f
Ian V Koeppe <ianvkoeppe@icloud.com>
2021-12-04 12:39:59 -0600

    Day 4

--- src/main/kotlin/io/Reader.kt
+++ src/main/kotlin/io/Reader.kt
@@ -4,5 +4,5 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 object Reader {
-  fun read(path: String): List<String> = Files.readAllLines(Paths.get(path))
+  fun read(path: String): List<String> = Files.readAllLines(Paths.get(path)).map { it.trim() }
 }
