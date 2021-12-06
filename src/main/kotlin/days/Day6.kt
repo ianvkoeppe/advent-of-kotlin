99900fab1c327b94f52f9efcfdacc853464c39be
Ian V Koeppe <ikoeppe@linkedin.com>
2021-12-06 11:50:53 -0600

    Include Both Day 6

--- src/main/kotlin/days/Day6.kt
+++ src/main/kotlin/days/Day6.kt
@@ -1,30 +1,56 @@
 package days
 
 object Day6 {
-  fun partOne(lines: List<String>): Long {
-    return simulate(parse(lines), 80)
+
+  private const val maxFishTime: Long = 8
+
+  object DynamicProgramming {
+    fun partOne(lines: List<String>): Long {
+      val fish = lines.first().split(",").map { it.toInt() }
+      return countDescendents(fish, 80)
+    }
+
+    fun partTwo(lines: List<String>): Long {
+      val fish = lines.first().split(",").map { it.toInt() }
+      return countDescendents(fish, 256)
+    }
+
+    private fun countDescendents(fish: List<Int>, days: Int): Long {
+      return fish.sumOf { countDescendents(it, days) }
+    }
+
+    private fun countDescendents(fish: Int, days: Int, counts: MutableMap<Pair<Int, Int>, Long> = mutableMapOf()): Long {
+      return counts.computeIfAbsent((fish to days)) { _ ->
+        val firstBorn = if (days - fish >= 1) 1 else 0
+        val descendents = firstBorn + (days - fish - 1) / 7
+        (0 until descendents).sumOf { countDescendents(8, days - fish - 1 - (it * 7), counts) }
+      } + 1
+    }
   }
 
-  fun partTwo(lines: List<String>): Long {
-    return simulate(parse(lines), 256)
+  object ReduceThenSimulate {
+    fun partOne(lines: List<String>): Long {
+      return simulate(parse(lines), 80)
+    }
+
+    fun partTwo(lines: List<String>): Long {
+      return simulate(parse(lines), 256)
+    }
+
+    private fun simulate(fish: List<Long>, days: Int): Long {
+      if (days == 0) return fish.sum()
+      return simulate(simulate(fish.toMutableList()), days - 1)
+    }
+
+    private fun simulate(fish: List<Long>): List<Long> {
+      return fish.indices.map { index ->
+        fish[(index + 1) % fish.size] + if (index == 6) fish[0] else 0
+      }
+    }
   }
 
   private fun parse(lines: List<String>): List<Long> {
     val fishCountByDay = lines.first().split(",").map { it.toLong() }.groupingBy { it }.eachCount()
-    return (0 until 9L).map { fishCountByDay.getOrDefault(it, 0).toLong() }
-  }
-
-  private fun simulate(fish: List<Long>, days: Int): Long {
-    if (days == 0) return fish.sum()
-    return simulate(simulate(fish.toMutableList()), days - 1)
-  }
-
-  private fun simulate(fish: List<Long>): List<Long> {
-    return fish.indices.map { index ->
-      when (index) {
-        6 -> fish[0] + fish[7]
-        else -> fish[(index + 1) % fish.size]
-      }
-    }
+    return (0..maxFishTime).map { fishCountByDay.getOrDefault(it, 0).toLong() }
   }
 }
--- src/test/kotlin/days/Day6Tests.kt
+++ src/test/kotlin/days/Day6Tests.kt
@@ -7,21 +7,25 @@ import org.junit.jupiter.api.Test
 class Day6Tests {
   @Test
   fun partOneExample() {
-    assertThat(Day6.partOne(Reader.read("src/test/resources/day6/example.txt"))).isEqualTo(5934)
+    assertThat(Day6.DynamicProgramming.partOne(Reader.read("src/test/resources/day6/example.txt"))).isEqualTo(5934)
+    assertThat(Day6.ReduceThenSimulate.partOne(Reader.read("src/test/resources/day6/example.txt"))).isEqualTo(5934)
   }
 
   @Test
   fun partOne() {
-    assertThat(Day6.partOne(Reader.read("src/test/resources/day6/problem.txt"))).isEqualTo(362346)
+    assertThat(Day6.DynamicProgramming.partOne(Reader.read("src/test/resources/day6/problem.txt"))).isEqualTo(362346)
+    assertThat(Day6.ReduceThenSimulate.partOne(Reader.read("src/test/resources/day6/problem.txt"))).isEqualTo(362346)
   }
 
   @Test
   fun partTwoExample() {
-    assertThat(Day6.partTwo(Reader.read("src/test/resources/day6/example.txt"))).isEqualTo(26984457539)
+    assertThat(Day6.DynamicProgramming.partTwo(Reader.read("src/test/resources/day6/example.txt"))).isEqualTo(26984457539)
+    assertThat(Day6.ReduceThenSimulate.partTwo(Reader.read("src/test/resources/day6/example.txt"))).isEqualTo(26984457539)
   }
 
   @Test
   fun partTwo() {
-    assertThat(Day6.partTwo(Reader.read("src/test/resources/day6/problem.txt"))).isEqualTo(1639643057051)
+    assertThat(Day6.DynamicProgramming.partTwo(Reader.read("src/test/resources/day6/problem.txt"))).isEqualTo(1639643057051)
+    assertThat(Day6.ReduceThenSimulate.partTwo(Reader.read("src/test/resources/day6/problem.txt"))).isEqualTo(1639643057051)
   }
 }
