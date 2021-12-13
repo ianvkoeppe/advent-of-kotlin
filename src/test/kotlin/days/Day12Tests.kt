b16bf4ab5f9099298aeb1c8d7f39236414ab7197
Ian V Koeppe <ikoeppe@linkedin.com>
2021-12-13 11:13:59 -0600

    Day 13

--- src/test/kotlin/days/Day12Tests.kt
+++ src/test/kotlin/days/Day12Tests.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 class Day12Tests {
   @Test
   fun partOneExamples() {
-    assertThat(Day12.partOne(Reader.read("src/test/resources/day12/example-1.txt"))).isEqualTo(10)
+    assertThat(Day12.partOne(Reader.read("src/test/resources/day12/example.txt"))).isEqualTo(10)
     assertThat(Day12.partOne(Reader.read("src/test/resources/day12/example-2.txt"))).isEqualTo(19)
     assertThat(Day12.partOne(Reader.read("src/test/resources/day12/example-3.txt"))).isEqualTo(226)
   }
@@ -19,7 +19,7 @@ class Day12Tests {
 
   @Test
   fun partTwoExample() {
-    assertThat(Day12.partTwo(Reader.read("src/test/resources/day12/example-1.txt"))).isEqualTo(36)
+    assertThat(Day12.partTwo(Reader.read("src/test/resources/day12/example.txt"))).isEqualTo(36)
     assertThat(Day12.partTwo(Reader.read("src/test/resources/day12/example-2.txt"))).isEqualTo(103)
     assertThat(Day12.partTwo(Reader.read("src/test/resources/day12/example-3.txt"))).isEqualTo(3509)
   }
