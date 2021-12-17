2be6e560a9a79fd73eb8744ca6ba10132854e83c
Ian V Koeppe <ianvkoeppe@icloud.com>
2021-12-16 20:08:56 -0600

    Day 17

---
+++ src/test/kotlin/days/Day17Tests.kt
@@ -0,0 +1,27 @@
+package days
+
+import io.Reader
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day17Tests {
+  @Test
+  fun partOneExamples() {
+    assertThat(Day17.partOne(Reader.read("src/test/resources/day17/example.txt"))).isEqualTo(45)
+  }
+
+  @Test
+  fun partOne() {
+    assertThat(Day17.partOne(Reader.read("src/test/resources/day17/problem.txt"))).isEqualTo(6903)
+  }
+
+  @Test
+  fun partTwoExample() {
+    assertThat(Day17.partTwo(Reader.read("src/test/resources/day17/example.txt"))).isEqualTo(112)
+  }
+
+  @Test
+  fun partTwo() {
+    assertThat(Day17.partTwo(Reader.read("src/test/resources/day17/problem.txt"))).isEqualTo(2351)
+  }
+}
