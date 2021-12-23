21ff7c3083c6f88715fe4865297143be73891493
Ian V Koeppe <ianvkoeppe@icloud.com>
2021-12-23 15:16:06 -0600

    Day 23

---
+++ src/test/kotlin/days/Day23Tests.kt
@@ -0,0 +1,27 @@
+package days
+
+import io.Reader
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day23Tests {
+  @Test
+  fun partOneExamples() {
+    assertThat(Day23.partOne(Reader.read("src/test/resources/day23/example.txt"))).isEqualTo(12521)
+  }
+
+  @Test
+  fun partOne() {
+    assertThat(Day23.partOne(Reader.read("src/test/resources/day23/problem.txt"))).isEqualTo(14467)
+  }
+
+  @Test
+  fun partTwoExample() {
+    assertThat(Day23.partTwo(Reader.read("src/test/resources/day23/example.txt"))).isEqualTo(44169)
+  }
+
+  @Test
+  fun partTwo() {
+    assertThat(Day23.partTwo(Reader.read("src/test/resources/day23/problem.txt"))).isEqualTo(48759)
+  }
+}
