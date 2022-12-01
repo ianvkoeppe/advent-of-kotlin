68ea08a8ba0f9980ff30cdaa88ff1aa348e32ab4
Ian V Koeppe <ikoeppe@linkedin.com>
2022-12-01 08:52:16 -0600

    Dec 1st, 2022

--- src/main/kotlin/y2022/Day1.kt
+++ src/main/kotlin/y2022/Day1.kt
@@ -1,11 +1,15 @@
 package y2022
 
 object Day1 {
-  fun partOne(lines: List<String>): Int {
-    return 0
+  fun partOne(lines: List<String>): Int? {
+    return findElfCalories(lines).maxOrNull()
   }
 
   fun partTwo(lines: List<String>): Int {
-    return 0
+    return findElfCalories(lines).sortedDescending().take(3).sum()
+  }
+
+  private fun findElfCalories(lines: List<String>): List<Int> {
+    return lines.joinToString("\n").split("\n\n").map { it.split("\n").sumOf { it.toInt() } }
   }
 }
--- src/test/kotlin/y2022/Day1Tests.kt
+++ src/test/kotlin/y2022/Day1Tests.kt
@@ -7,21 +7,21 @@ import org.junit.jupiter.api.Test
 class Day1Tests {
   @Test
   fun partOneExample() {
-    assertThat(Day1.partOne(Reader.readExample(2022, 1))).isEqualTo(0)
+    assertThat(Day1.partOne(Reader.readExample(2022, 1))).isEqualTo(24000)
   }
 
   @Test
   fun partOne() {
-    assertThat(Day1.partOne(Reader.readProblem(2022, 1))).isEqualTo(0)
+    assertThat(Day1.partOne(Reader.readProblem(2022, 1))).isEqualTo(72017)
   }
 
   @Test
   fun partTwoExample() {
-    assertThat(Day1.partTwo(Reader.readExample(2022, 1))).isEqualTo(0)
+    assertThat(Day1.partTwo(Reader.readExample(2022, 1))).isEqualTo(45000)
   }
 
   @Test
   fun partTwo() {
-    assertThat(Day1.partTwo(Reader.readProblem(2022, 1))).isEqualTo(0)
+    assertThat(Day1.partTwo(Reader.readProblem(2022, 1))).isEqualTo(212520)
   }
 }
