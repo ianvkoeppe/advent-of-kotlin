bdd5761b05ba479ba781b04e14f922fbf2fd94e9
Ian V Koeppe <ianvkoeppe@icloud.com>
2022-12-15 20:14:44 -0600

    Dec 16th, 2022

---
+++ src/test/kotlin/y2022/Day16Tests.kt
@@ -0,0 +1,27 @@
+package y2022
+
+import io.Reader
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day16Tests {
+  @Test
+  fun partOneExample() {
+    assertThat(Day16.partOne(Reader.readExample(2022, 16))).isEqualTo(1651)
+  }
+
+  @Test
+  fun partOne() {
+    assertThat(Day16.partOne(Reader.readProblem(2022, 16))).isEqualTo(1986)
+  }
+
+  @Test
+  fun partTwoExample() {
+    assertThat(Day16.partTwo(Reader.readExample( 2022, 16))).isEqualTo(1707)
+  }
+
+  @Test
+  fun partTwo() {
+    assertThat(Day16.partTwo(Reader.readProblem(2022, 16))).isEqualTo(2464)
+  }
+}
