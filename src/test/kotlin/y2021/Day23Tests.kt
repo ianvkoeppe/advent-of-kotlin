d90fdda2144209db2ebb2629ec4f72930dbd87ce
Ian V Koeppe <ianvkoeppe@icloud.com>
2023-10-28 23:06:36 -0500

    Reenable Some Tests & Profile Slow Ones

--- src/test/kotlin/y2021/Day23Tests.kt
+++ src/test/kotlin/y2021/Day23Tests.kt
@@ -2,7 +2,6 @@ package y2021
 
 import io.Reader
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class Day23Tests {
@@ -12,19 +11,16 @@ class Day23Tests {
   }
 
   @Test
-  @Disabled("Slow")
   fun partOne() {
     assertThat(Day23.partOne(Reader.readProblem(2021, 23))).isEqualTo(14467)
   }
 
   @Test
-  @Disabled("Slow")
   fun partTwoExample() {
     assertThat(Day23.partTwo(Reader.readExample(2021, 23))).isEqualTo(44169)
   }
 
   @Test
-  @Disabled("Slow")
   fun partTwo() {
     assertThat(Day23.partTwo(Reader.readProblem(2021, 23))).isEqualTo(48759)
   }
