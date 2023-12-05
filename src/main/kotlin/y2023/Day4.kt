f60d34edbd692fcdc6a1b331f4f2b63ddca11090
Ian V Koeppe <ianvkoeppe@icloud.com>
2023-12-05 13:39:23 -0600

    [Dec 4th, 2023] Parse to single list of winners.

--- src/main/kotlin/y2023/Day4.kt
+++ src/main/kotlin/y2023/Day4.kt
@@ -3,27 +3,23 @@ package y2023
 import kotlin.math.pow
 
 object Day4 {
-  private data class Card(val id: Int, val winners: Set<Int>)
-
-  fun partOne(lines: List<String>): Int = lines.map(::parse).sumOf { card -> 2.0.pow(card.winners.size - 1).toInt() }
+  fun partOne(lines: List<String>): Int = lines.map(::parse).sumOf { winners -> 2.0.pow(winners - 1).toInt() }
 
   fun partTwo(lines: List<String>): Int {
     val cards = lines.map(::parse)
 
     return cards
-      .fold(cards.associate { it.id to 1 }) { wins, card ->
-        val winners = (1..card.winners.size).map(card.id::plus).toSet()
-        wins + winners.associateWith { winner -> wins.getValue(winner) + wins.getValue(card.id) }
+      .foldIndexed(cards.withIndex().associate { it.index to 1 }) { index, wins, winners ->
+        wins + (1..winners).map(index::plus).associateWith { winner -> wins.getValue(winner) + wins.getValue(index) }
       }
       .values
       .sum()
   }
 
-  private fun parse(line: String): Card {
-    val id = line.substringAfter("Card ").substringBefore(": ").trim().toInt()
+  private fun parse(line: String): Int {
     val winners = line.substringAfter(": ").substringBefore(" | ").splitNumbers()
     val numbers = line.substringAfter(" | ").splitNumbers()
-    return Card(id, numbers.filter(winners::contains).toSet())
+    return numbers.count(winners::contains)
   }
 
   private fun String.splitNumbers(): List<Int> = this.trim().split("\\s+".toRegex()).map(String::toInt)
